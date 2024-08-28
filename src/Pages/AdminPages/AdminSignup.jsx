6982c3a31ae557cc53de3284f4266978947b0e1c
Innocent866 <igoldima@gmail.com>
2024-08-28 15:46:58 -0700

    updated

--- src/Pages/AdminPages/AdminSignup.jsx
+++ src/Pages/AdminPages/AdminSignup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../../style/Auth/Signup.css";
 import { LiaFileUploadSolid } from "react-icons/lia";
-import SignupImage from "../../assets/Home/Hero/Mouth Throne.jpg";
+import SignupImage from "../../assets/Home/Hero/Mouththrone.jpg";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
